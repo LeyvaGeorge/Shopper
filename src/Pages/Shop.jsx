fe81c6e8a21ac2ac3340fac65bf2c68a92851027
George Leyva <leyvageorge@hotmail.com>
2023-12-04 15:59:39 -0800

    added items

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Hero } from '../Components/Hero/Hero'
+import { Popular } from '../Components/Popular/Popular'
 
 // Home Page
 export const Shop = () => {
   return (
     <div>
         <Hero/>
+        <Popular/>
     </div>
   )
 }
